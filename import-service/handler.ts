d82293c0ace0231e4a05b896641ad082fe59b64d
Valentin Grinkevich <grinkevich.vv@gmail.com>
2020-11-22 07:47:46 +0300

    minor improvements

--- import-service/handler.ts
+++ import-service/handler.ts
@@ -1,13 +1,15 @@
 import { APIGatewayProxyHandler, S3Handler } from 'aws-lambda'
+import 'source-map-support/register'
 
 import S3 from 'aws-sdk/clients/s3'
 import csvParser from 'csv-parser'
 
-import 'source-map-support/register'
+
+import { corsHeaders } from './constants/headers'
 
 const BUCKET = 'imported-csv'
 
-export const catalogUpload: APIGatewayProxyHandler = (event) => {
+export const catalogUpload: APIGatewayProxyHandler = async (event) => {
   const catalogName = event.queryStringParameters.name
   const catalogPath = `uploaded/${catalogName}`
   const s3 = new S3({ region: 'eu-west-1' })
@@ -18,17 +20,25 @@ export const catalogUpload: APIGatewayProxyHandler = (event) => {
     ContentType: 'text/csv'
   }
 
-  return new Promise((resolve, reject)=>{
-    s3.getSignedUrl('putObject', params, (err, url)=>{
-      if(err) return reject(err)
+  try{
+    const url = await s3.getSignedUrlPromise('putObject', params)
+    return {
+      statusCode: 200,
+      headers: corsHeaders,
+      body: url
+    }
+  } catch(error){
+    return {
+      statusCode: 500,
+      headers: corsHeaders,
+      body: JSON.stringify({
+        message: error.message,
+      }),
+    }
+  } finally {
+    console.log(event)
+  }
 
-      resolve({
-        statusCode: 200,
-        headers: { 'Access-Control-Allow-Origin': '*' },
-        body: url
-      })
-    })
-  })
 }
 
 export const catalogParse: S3Handler = (event) => {
